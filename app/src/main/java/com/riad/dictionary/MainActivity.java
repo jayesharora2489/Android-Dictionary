f8ca7e11d73829d7b1e952373943236f6cfa025a
Jayesh Arora <jayesh.arora98@gmail.com>
2019-07-02 21:30:21 +0530

    first commit

---
+++ app/src/main/java/com/riad/dictionary/MainActivity.java
@@ -0,0 +1,91 @@
+package com.riad.dictionary;
+
+import android.content.Intent;
+import android.database.SQLException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    SearchView search;
+
+    static DatabaseHelper myDbHelper;
+    static boolean databaseOpened=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        search =  (SearchView) findViewById(R.id.search_view);
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                search.setIconified(false);
+
+            }
+        });
+
+
+        myDbHelper = new DatabaseHelper(this);
+
+        if(myDbHelper.checkDataBase())
+        {
+            openDatabase();
+
+        }
+        else
+        {
+            LoadDatabaseAsync task = new LoadDatabaseAsync(MainActivity.this);
+            task.execute();
+        }
+
+    }
+
+
+    protected static void openDatabase()
+    {
+        try {
+            myDbHelper.openDataBase();
+            databaseOpened=true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_main; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (id == R.id.action_exit) {
+            System.exit(0);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/riad/dictionary/fragments/FragmentDefinition.java
@@ -0,0 +1,25 @@
+package com.riad.dictionary.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.riad.dictionary.R;
+
+public class FragmentDefinition extends Fragment {
+    public FragmentDefinition() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_definition,container, false);//Inflate Layout
+
+
+        return view;
+    }
+}
